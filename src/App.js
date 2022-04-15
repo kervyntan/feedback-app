1a91355488f08cb6a343477e41b7357109c5d28f
Kervyn <kervyntan@gmail.com>
2022-04-16 00:32:23 +0800

    Completed first 4 videos of first section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const title = "Welcome to my first app";
+    const body = "cool beans is my favourite";
+    const comments = [
+        {id : 1, text : 'Comment one'},
+        {id : 2, text : 'Comment two'},
+    ]
+    return (
+        <div className = 'container'>
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+
+        <div className= 'comments'>
+            <h3>Comments ( {comments.length} )</h3>
+
+            <ul>
+                {comments.map((comment,index) => {
+                    return <li key={index}> {comment.text} </li>
+                })}
+            </ul>
+        </div>
+
+        </div>
+    )
 }
 
 export default App;
